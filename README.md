6f33322ec2d0bed78e9f1cccbe322bc40bfec0b2
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2016-04-09 04:25:19 -0500

    uploaded script

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-The watch.js script will periodically ping the ports and check for the proper responses. If it doesn't receive a proper response, it will alert you. If you don't turn it off but the server does not recover, it will keep alerting you.
+The watch.js script will periodically ping the ports and check for the proper responses. If it doesn't receive a proper response, it will alert you. If you don't turn it off but the server does not recover, it will keep alerting you. When this is the case, it'd be in your best interest to turn it off before [textbelt](http://textbelt.com/) gets angry at you. See [textbelt](http://textbelt.com/)'s limits and restrictions for more info.
 
 Configuration is stored in config.json. Configuration values are:
 
   *  phone: the phone number to send messages to using [textbelt](http://textbelt.com/)
   *  checkFrequency: the number of seconds to wait between status checks. It is recommended that you do not set this below 600 (default) or textbelt and bomb.cs will both be angry with you.
   *  verbose: should we pour our soul into stdout? Defaults to true.
-  *  targets: an array of url + port combos (aka: valid urls) that will be pinged to check for upness. Set to null to disable.
-  *  scoreboard: the url of the scoreboard, which will be scraped and its emailed checked for validity. Rows already checked will not be rechecked. Set to null to disable.
+  *  targets: an array of hostname + port combos (i.e. "bomb.cs:12345") that will be pinged to check for upness. Set to empty array to disable.
+  *  scoreboard: the complete url (+ protocol) of the scoreboard.
 
 That's about it. Runs on windows, mac, or linux, as long as you keep the console window open and are connected to the university network/VPN.
 
---
+++ watch.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env "node --harmony"
+/*jshint esnext:true */
+
+/*
+ * @author Bernard Dickens (https://bernard.ergodark.com)
+ */
+
+"use strict";
+
+var shush   = require('shush');
+var request = require('request');
+var tcpp    = require('tcp-ping');
+
+var config  = shush('config.json');
+
+const PHONE      = config.phone;
+const FREQUENCY  = config.checkFrequency;
+const VERBOSE    = config.verbose;
+const TARGETS    = config.targets;
+const SCOREBOARD = config.scoreboard;
+
+var printIfDebug = function(...msg)
+{
+    if(VERBOSE)
+        console.log(...msg);
+};
+
+var panic = function(msg, fn)
+{
+    printIfDebug('>> PANIC!');
+    printIfDebug(`Sending text "${msg}" to #${PHONE}`);
+
+    request.post('http://textbelt.com/text', { number: PHONE, message: msg }, function(err, response, body)
+    {
+        printIfDebug('Textbelt send attempt:');
+        printIfDebug('Error (nothing is good):', err);
+        printIfDebug('HTTP status code:', response.statusCode);
+        printIfDebug('Raw response body:', body);
+
+        var bodyJSON = JSON.parse(body);
+
+        printIfDebug('Response JSON:', bodyJSON);
+
+        if(!bodyJSON.success)
+            console.log('>> ERROR: Failed to send text via Textbelt!');
+
+        fn();
+    });
+};
+
+var loop = function(fn)
+{
+    return function()
+    {
+        printIfDebug('Next check will occur in about ${FREQUENCY} seconds');
+        setTimeout(fn, FREQUENCY);
+    };
+};
+
+var main = function()
+{
+    printIfDebug('Performing project 2 check...');
+    printIfDebug('Making HTTP GET request to scoreboard @ ${SCOREBOARD}');
+
+    request(SCOREBOARD, function(err, response/*, body*/)
+    {
+        printIfDebug('Request made');
+        printIfDebug('Error: (nothing is good)', err);
+        printIfDebug('HTTP status code:', response.statusCode);
+
+        if(!err && response.statusCode == 200)
+        {
+            printIfDebug('Response from server received successfully! (status code 200)');
+
+            TARGETS.forEach(function(target)
+            {
+                printIfDebug('Preparing to probe target ${target}');
+
+                var components = target.split(':');
+                var host = components[0];
+                var port = components[1];
+
+                printIfDebug('Host:', host);
+                printIfDebug('Port:', port);
+
+                tcpp.probe(host, port, function(err, available)
+                {
+                    printIfDebug('Probe complete!');
+                    printIfDebug('Error (nothing is good):', err);
+                    printIfDebug('Available:', available);
+
+                    if(err || !available)
+                        panic(`TARGET FAILURE! Could not access ${host}:${port}!!!`, loop(main));
+                });
+            });
+        }
+
+        else
+            panic(`SCOREBOARD FAILURE! Problem accessing scoreboard (HTTP ${response.statusCode})!!!`, loop(main));
+    });
+};
+
+printIfDebug('CONFIG DUMP:', config);
+main();
